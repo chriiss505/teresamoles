fe10c279c04ff21d12cb7c4c82097626bc2cd754
cristina <cristina.sanchez@vixtechnology.com>
2026-01-11 22:27:19 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: "https://chriiss505.github.io",
+  base: "/teresamoles", // Solo si es repositorio de proyecto, no user/organization pages
 });
