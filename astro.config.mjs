2ed79fce5b44b5f2e1810dd3337878b14eb4898c
cristina <cristina.sanchez@vixtechnology.com>
2026-01-12 17:59:55 +0100

    new try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-
 export default defineConfig({
-  site: "https://github.com/chriiss505/teresamoles",
+  site: "https://chriiss505.github.io/teresamoles",
+  base: "/teresamoles/",
   output: "static",
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,js,ts,jsx,tsx}", // todos tus archivos
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   theme: { extend: {} },
   plugins: [require("@midudev/tailwind-animations")],
 };
