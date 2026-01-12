48bdc2980e8033e03577a92fa8bcbf33a16ee9f1
cristina <cristina.sanchez@vixtechnology.com>
2026-01-12 17:30:04 +0100

    more fixes for deploying to ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://chriiss505.github.io",
+  site: "https://github.com/chriiss505/teresamoles",
   output: "static",
-  integrations: [tailwind()],
 });
