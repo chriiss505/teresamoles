f306b8e551ee34f99eaf5103ddd27bcea22ef623
cristina <cristina.sanchez@vixtechnology.com>
2026-01-21 17:00:43 +0100

    changed to www.clinicateresamoles.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
+
 export default defineConfig({
-  site: "https://chriiss505.github.io/teresamoles",
-  base: "/teresamoles/",
+  site: "https://clinicateresamoles.com",
   output: "static",
 });
