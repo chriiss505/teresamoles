baf7990fb000cc4ec0b069478e6b84c19255e737
cristina <cristina.sanchez@vixtechnology.com>
2026-01-27 22:11:30 +0100

    Seo fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://clinicateresamoles.com",
   output: "static",
+  integrations: [sitemap()],
 });
